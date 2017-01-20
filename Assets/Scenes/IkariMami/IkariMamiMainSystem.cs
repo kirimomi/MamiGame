dc875f4a939c45de3e79a373eadd3869890eeb7b
kirimomi <kirimomi@gmail.com>
2017-01-20 18:01:30 +0900

    にっこりマミを選ぶゲーム

---
+++ Assets/Scenes/IkariMami/IkariMamiMainSystem.cs
@@ -0,0 +1,280 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class IkariMamiMainSystem : MonoBehaviour
+{
+
+	public GameObject MamiPrefab;
+
+	public static AudioSource Audio;
+	public AudioClip BGM;
+	public AudioClip END;
+
+	public AudioClip Explosion;
+
+	public GameObject Telop;
+
+	public Text TimeText;
+	public Text RecordText;
+	public GameObject ReplayButton;
+
+	public GameObject LocatorRoot;
+	public Transform [] Locators;
+
+	struct WaveDef {
+		public int MamiNum;
+		public int NormalNum;
+		public int IkariNum;
+		public int NorioNum;
+	}
+
+	int m_wave = 1;
+	const int WAVE_MAX = 10;
+
+	float m_time = 0;
+
+	WaveDef GetWaveDefs(int wave){
+		Assert.IsTrue(1<=wave && wave <=WAVE_MAX);
+
+		WaveDef ret = new WaveDef();
+		switch (wave) {
+		case 1:
+			ret.MamiNum = 3;
+			ret.NormalNum = 1;
+			ret.IkariNum = 0;
+			ret.NorioNum = 0;
+			break;
+		case 2:
+			ret.MamiNum = 3;
+			ret.NormalNum = 1;
+			ret.IkariNum = 2;
+			ret.NorioNum = 0;
+			break;
+		case 3:
+			ret.MamiNum = 5;
+			ret.NormalNum = 2;
+			ret.IkariNum = 0;
+			ret.NorioNum = 1;
+			break;
+		case 4:
+			ret.MamiNum = 5;
+			ret.NormalNum = 1;
+			ret.IkariNum = 1;
+			ret.NorioNum = 1;
+			break;
+		case 5:
+			ret.MamiNum = 8;
+			ret.NormalNum = 0;
+			ret.IkariNum = 0;
+			ret.NorioNum = 4;
+			break;
+		case 6:
+			ret.MamiNum = 8;
+			ret.NormalNum = 2;
+			ret.IkariNum = 3;
+			ret.NorioNum = 0;
+			break;
+		case 7:
+			ret.MamiNum = 8;
+			ret.NormalNum = 1;
+			ret.IkariNum = 4;
+			ret.NorioNum = 0;
+			break;
+		case 8:
+			ret.MamiNum = 11;
+			ret.NormalNum = 3;
+			ret.IkariNum = 5;
+			ret.NorioNum = 0;
+			break;
+		case 9:
+			ret.MamiNum = 11;
+			ret.NormalNum = 4;
+			ret.IkariNum = 2;
+			ret.NorioNum = 1;
+			break;
+		case 10:
+			ret.MamiNum = 11;
+			ret.NormalNum = 0;
+			ret.IkariNum = 11;
+			ret.NorioNum = 0;
+			break;
+		}
+
+		return ret;
+
+	}
+
+
+
+	float m_fastestTime;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Audio = GetComponent<AudioSource> ();
+		Audio.clip = BGM;
+		Audio.Play ();
+
+		Telop.SetActive (false);
+		ReplayButton.SetActive (false);
+
+		m_fastestTime = LoadFastestTime ();
+		if (100f <= m_fastestTime ) {
+			RecordText.text = "";
+		} else {
+			RecordText.text = "Record : " + m_fastestTime.ToString ("00.00");
+		}
+
+		MakeWave (m_wave);
+
+		StartCoroutine (Main ());
+
+	}
+
+
+	IEnumerator Main(){
+		while (true) {
+			if (Input.GetMouseButtonDown (0)) {
+				Vector2 worldPoint = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+
+				//Debug.Log ("worldPoint :x" + worldPoint.x + "y:" + worldPoint.y);
+
+				//タッチをした位置にオブジェクトがあるかどうかを判定
+				RaycastHit2D hit = Physics2D.Raycast (worldPoint, Vector2.zero);
+				if (hit) {
+					Bounds rect = hit.collider.bounds;
+					//オブジェクトがある場合は、そのオブジェクトを消している
+					if (rect.Contains (worldPoint)) {
+						if (hit.collider.gameObject.tag == "IkariMami_Mami") {
+							hit.collider.gameObject.GetComponent<IkariMami_Mami> ().Touched ();
+						}
+					}
+				}
+			}
+
+
+			TimeText.text = m_time.ToString ("00.00");
+
+
+			if (CheckClear ()) {
+				Debug.Log ("Cleared!");
+				yield return ClearWave ();
+				m_wave++;
+				if (WAVE_MAX < m_wave) {
+					yield return ClearGame ();
+				} else {
+					MakeWave (m_wave);
+				}
+			}
+
+			m_time += Time.deltaTime;
+			yield return null;
+		}
+	}
+
+
+	GameObject [] m_mamis;
+
+	bool CheckClear(){
+
+		/*if (m_mamis == null) {
+			return false;
+		}*/
+		for(int i=0; i < m_mamis.Length; i++){
+			if (m_mamis [i]) {
+				if (m_mamis [i].GetComponent<IkariMami_Mami> ().Mode != IkariMami_Mami.MamiMode.Warai) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+
+	void MakeWave(int wave){
+		LocatorRoot.transform.position = Vector3.zero;
+
+		WaveDef def = GetWaveDefs (wave);
+		m_mamis = new GameObject[def.MamiNum];
+		for(int i = 0; i < def.MamiNum; i++){
+			m_mamis[i] = Instantiate (MamiPrefab, Locators[i].position, Quaternion.identity);
+			m_mamis [i].transform.parent = LocatorRoot.transform;
+		}
+
+	}
+
+	const float CLEAR_MOVE_SPEED = 20f;
+
+	//WAVEクリア演出
+	IEnumerator ClearWave(){
+
+		Telop.SetActive (true);
+		Telop.GetComponent<Text> ().text = "Good!";
+
+		yield return new WaitForSeconds(0.2f);//少し待つ
+		Audio.PlayOneShot (Explosion);
+	
+		while (LocatorRoot.transform.position.y < 9) {
+			LocatorRoot.transform.position += Vector3.up * CLEAR_MOVE_SPEED * Time.deltaTime;
+			yield return null;
+		}
+
+		//消す
+		for(int i=0; i < m_mamis.Length; i++){
+			if (m_mamis [i]) {
+				Destroy (m_mamis [i].gameObject);
+			}
+		}
+
+		Telop.SetActive (false);
+	}
+
+	IEnumerator ClearGame(){
+
+		yield return new WaitForSeconds(0.2f);//少し待つ
+
+		Audio.Stop ();
+		Audio.PlayOneShot (END);
+		ReplayButton.SetActive (true);
+
+
+		Telop.GetComponent<Text> ().text = "Cleared!";
+		Telop.SetActive(true);
+
+
+		if (m_time < m_fastestTime) {
+			SaveFastestTime (m_time);
+		}
+
+		while (true) {
+			yield return null;
+		}
+	}
+
+
+
+
+	public void OnButtonReplay ()
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+	}
+
+
+	const string FASTEST_TIME_KEY = "fastestTime";
+
+	void SaveFastestTime (float time)
+	{
+		PlayerPrefs.SetFloat (FASTEST_TIME_KEY, time);
+		PlayerPrefs.Save ();
+	}
+
+	float LoadFastestTime ()
+	{
+		return PlayerPrefs.GetFloat (FASTEST_TIME_KEY, 9999999f);
+	}
+
+}
---
+++ Assets/Scenes/IkariMami/IkariMami_Mami.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IkariMami_Mami : MonoBehaviour {
+
+	public Sprite SpriteNormal;
+	public Sprite SpriteWarai;
+	public Sprite SpriteIkari;
+	public Sprite SpriteNorio;
+
+	public SpriteRenderer MamiFace;
+
+	public GameObject SerihuPrefab;
+	public AudioClip SeGood;
+	public AudioClip SeBad;
+	public AudioClip SeNorio;
+
+	public enum MamiMode{
+		Warai,
+		Normal,
+		Ikari,
+		Norio,
+	}
+
+	MamiMode m_mode = MamiMode.Normal;
+
+	public MamiMode Mode{get {return m_mode;} set{ m_mode = value; }}
+
+
+	// Use this for initialization
+	void Start () {
+		if (Random.value < 0.1f) {
+			m_mode = MamiMode.Ikari;
+		} else if(Random.value < 0.2f){
+			m_mode = MamiMode.Warai;
+		}else if(Random.value < 0.4f){
+			m_mode = MamiMode.Norio;
+		}
+
+		SetFaceSprite ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void Touched(){
+
+		if (m_mode == MamiMode.Norio) {
+			IkariMamiMainSystem.Audio.PlayOneShot (SeNorio);
+			MakeSerihu ("どぅぇ");
+			Destroy (gameObject);
+		}
+
+
+		switch(m_mode){
+		case MamiMode.Warai:
+			m_mode = MamiMode.Ikari;
+			IkariMamiMainSystem.Audio.PlayOneShot (SeBad);
+			MakeSerihu ("あーれー");
+			break;
+		case MamiMode.Normal:
+			m_mode = MamiMode.Warai;
+			IkariMamiMainSystem.Audio.PlayOneShot (SeGood);
+			MakeSerihu ("マミーン");
+			break;
+		case MamiMode.Ikari:
+			m_mode = MamiMode.Normal;
+			IkariMamiMainSystem.Audio.PlayOneShot (SeGood);
+			MakeSerihu ("マミーン");
+			break;
+		}
+		SetFaceSprite ();
+	}
+
+	void SetFaceSprite(){
+		//Debug.Log ("hoge");
+		switch (m_mode) {
+		case MamiMode.Warai:
+			MamiFace.sprite = SpriteWarai;
+			break;
+		case MamiMode.Normal:
+			MamiFace.sprite = SpriteNormal;
+			break;
+		case MamiMode.Ikari:
+			MamiFace.sprite = SpriteIkari;
+			break;
+		case MamiMode.Norio:
+			MamiFace.sprite = SpriteNorio;
+			break;
+		}
+	}
+
+	void MakeSerihu(string txt){
+		GameObject obj = Instantiate (SerihuPrefab, transform.position ,Quaternion.identity);
+		obj.transform.FindChild("TextMesh").GetComponent<TextMesh>().text = txt;
+	}
+
+}
