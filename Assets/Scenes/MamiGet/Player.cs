e45e06c02b445a4c6b8e766fd7fd75820dd0fbec
nmatsumura <nmatsumura@creatures.jp>
2022-05-05 10:34:07 +0900

    ノリオの操作を変更 タップした方向に移動するように

--- Assets/Scenes/MamiGet/Player.cs
+++ Assets/Scenes/MamiGet/Player.cs
@@ -5,29 +5,29 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-	// Use this for initialization
-	void Start ()
-	{
-		
-	}
-	
-	// Update is called once per frame
+    // Use this for initialization
+    void Start()
+    {
 
-	//const float PLAYER_MAX_SPD = 20.0f;
-	const float PLAYER_MAX_SPD = 200.0f;
+    }
 
-	const float PLAYER_SPD = 2.0f;
+    // Update is called once per frame
 
-	const float SCREEN_EDGE_X = 2.8f;
+    //const float PLAYER_MAX_SPD = 20.0f;
+    const float PLAYER_MAX_SPD = 200.0f;
 
-	//const float SCALE_PER_MAMI = 0.18f;
-	const float SCALE_PER_MAMI = 0.3f;
+    const float PLAYER_SPD = 2.0f;
 
-	void Update ()
-	{
+    const float SCREEN_EDGE_X = 2.8f;
 
-		//===============普通に操作する実験===============
-		/*
+    //const float SCALE_PER_MAMI = 0.18f;
+    const float SCALE_PER_MAMI = 0.3f;
+
+    void Update()
+    {
+
+        //===============普通に操作する実験===============
+#if false
 		Vector3 spd = Vector3.zero;
 		if (Input.GetKey ("up")) {
 			spd += Vector3.up * PLAYER_MAX_SPD;
@@ -42,43 +42,66 @@ public class Player : MonoBehaviour
 			spd += Vector3.right * PLAYER_MAX_SPD;
 		}
 		transform.position =  transform.position + spd * Time.deltaTime;
-		*/
+#endif
 
-		//===============クリックしたところに移動実験===============
-		/*
+#if false
+        //===============クリックしたところに移動実験===============
 		if(Input.GetMouseButton(0)){
 			Vector3 mousePos =Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			Vector3 targetPos = mousePos + Vector3.up * 1.5f;
 			targetPos.z = 0;
 			transform.position = Vector3.MoveTowards(transform.position, targetPos, PLAYER_MAX_SPD * Time.deltaTime);
-		}*/
-
-		//===============1ボタン操作実験===============
-		if (0 < MainSystem.Counter) {
-			Vector3 spd = Vector3.zero;
-			if (Input.GetMouseButton (0)) {
-				spd.x = PLAYER_SPD;
-			} else {
-				spd.x = -PLAYER_SPD;
-			}
-			Vector3 targetPos = transform.position + spd * Time.deltaTime;
-
-			float limitX = SCREEN_EDGE_X - GetPlayerRadius ();
-			targetPos.x = Mathf.Clamp (targetPos.x, -limitX, limitX);
-			transform.position = targetPos;
 		}
+#endif
 
-		//プイレイヤーのスケール
+#if false
+        //===============1ボタン操作実験===============
+        if (0 < MainSystem.Counter)
+        {
+            Vector3 spd = Vector3.zero;
+            if (Input.GetMouseButton(0))
+            {
+                spd.x = PLAYER_SPD;
+            }
+            else
+            {
+                spd.x = -PLAYER_SPD;
+            }
+            Vector3 targetPos = transform.position + spd * Time.deltaTime;
 
-		//transform.localScale = Vector3.one + Vector3.one * SCALE_PER_MAMI * Mathf.Sqrt (MainSystem.Score);
-		transform.localScale = Vector3.one * 0.7f + Vector3.one * SCALE_PER_MAMI * Mathf.Sqrt (MainSystem.Score);
+            float limitX = SCREEN_EDGE_X - GetPlayerRadius();
+            targetPos.x = Mathf.Clamp(targetPos.x, -limitX, limitX);
+            transform.position = targetPos;
+        }
+#endif
 
-	}
+        //===============タップした方向へ移動する===============
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 spd = Vector3.zero;
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            spd.x = PLAYER_SPD * (mousePos.x < transform.position.x ? -1 : 1);
+            Vector3 targetPos = transform.position + spd * Time.deltaTime;
+
+            float limitX = SCREEN_EDGE_X - GetPlayerRadius();
+            targetPos.x = Mathf.Clamp(targetPos.x, -limitX, limitX);
+            transform.position = targetPos;
+        }
 
 
-	float GetPlayerRadius(){
-		float radius = GetComponent<CircleCollider2D> ().radius * transform.localScale.x;
-		//Debug.Log ("radius is " + radius);
-		return radius;
-	}
+
+        //プイレイヤーのスケール
+
+        //transform.localScale = Vector3.one + Vector3.one * SCALE_PER_MAMI * Mathf.Sqrt (MainSystem.Score);
+        transform.localScale = Vector3.one * 0.7f + Vector3.one * SCALE_PER_MAMI * Mathf.Sqrt(MainSystem.Score);
+
+    }
+
+
+    float GetPlayerRadius()
+    {
+        float radius = GetComponent<CircleCollider2D>().radius * transform.localScale.x;
+        //Debug.Log ("radius is " + radius);
+        return radius;
+    }
 }
